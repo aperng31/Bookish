aea5b426e30e5d486b95e7d54eea5ce228d565df
Tomas Kim <tk0885@gmail.com>
2022-12-13 18:51:16 -0500

    added bookshelf feature

--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { getBooks, createBook, updateBook, deleteBook } from '../redux/userSlice'
-import BookCard from './BookCard.jsx'
-import '../styles/books.scss'
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  getBooks,
+  createBook,
+  updateBook,
+  deleteBook,
+} from '../redux/userSlice';
+import BookCard from './BookCard.jsx';
+import '../styles/books.scss';
 
-function Header(props) {
-  const bookData = useSelector(state => state.book.bookData);
-  const userData = useSelector(state => state.book.userData);
+function Header() {
+  const bookData = useSelector((state) => state.book.bookData);
+  const userData = useSelector((state) => state.book.userData);
   const temp = 'helloworld';
   return (
     <header>
-      <h3>Welcome <span>{ props.userData.name }</span></h3>
-      
+      <h3>
+        Welcome <span></span>
+      </h3>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
